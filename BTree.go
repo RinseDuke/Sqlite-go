40c01f885447530f33b8ef4ca7fd1a278939ad16
RinseDuke <eastocean86sd@outlook.com>
2026-05-26 19:27:21 +0800

    improve:finish treeInsert function

--- BTree.go
+++ BTree.go
@@ -1,5 +1,7 @@
 package main
 
+import "bytes"
+
 //Btree struct
 type BTree struct {
 	//root pointer
@@ -11,11 +13,33 @@ type BTree struct {
 }
 
 //Btree node insert
-func treeInsert(tree *BTree, node BNode, key []byte, val []byte) {
+func treeInsert(tree *BTree, node BNode, key []byte, val []byte) BNode {
+	//init the BNode
+	new := BNode(make([]byte, BTREE_PAGE_SIZE*2))
 	//find if the key already exists in the node
 	idx := nodeLookupLE(node, key)
 	//jude if the node is a leaf node or not
 	switch node.btype() {
-
+	case BNODE_LEAF:
+		//if the key already exists,update the value
+		if bytes.Equal(node.getkey(idx), key) {
+			leafUpdate(new, node, idx, key, val)
+		} else {
+			//if the key does not exist,insert the key and value into the node
+			leafInsert(new, node, idx, key, val)
+		}
+	case BNODE_NODE:
+		//recursively insertion to the child node
+		kptr := node.getPtr(idx)
+		//first insert the key and value into the child node
+		knode := treeInsert(tree, tree.get(kptr), key, val)
+		//then check if the child node is split or not
+		//after insertion,split the node
+		nsplit, split := nodeSplit3(knode)
+		//deallocation the old kid node
+		tree.del(kptr)
+		//update the kid links
+		nodeRepalceKidN(tree, new, node, idx, split[:nsplit]...)
 	}
+	return new
 }
